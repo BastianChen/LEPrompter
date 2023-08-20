d40614e0d0f81e2717c9dc0fe7a814b6ed279498
Ben <595470658@qq.com>
2023-08-20 16:57:24 +0800

    update

--- local_configs/_base_/schedules/schedule_160k.py
+++ local_configs/_base_/schedules/schedule_160k.py
@@ -5,6 +5,6 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=160000)
-checkpoint_config = dict(by_epoch=False, interval=160000)
+checkpoint_config = dict(by_epoch=False, interval=16000)
 # evaluation = dict(interval=16000, metric='mIoU', pre_eval=True)
 evaluation = dict(interval=16000, metric=['mIoU', 'mFscore'])
